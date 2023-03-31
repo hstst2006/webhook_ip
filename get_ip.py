3addb938652ae3235960b9c68ddb1cf7bd3b700a
Håvard Skordal Thorsen <hstst2006@gmail.com>
2023-03-31 15:05:15 +0200

    Changed some line spacing

--- get_ip.py
+++ get_ip.py
@@ -19,12 +19,18 @@ while(attempts < 300):
         s.connect(("8.8.8.8", 1))
     except:
         pass
+
     #Read the socket's own address, if there is no address sleep and loop again
     #Else POST ip to webhook
     my_IP = s.getsockname()[0]
+
     if str(my_IP) != "0.0.0.0":
+
+	# Obtain and format current time as string
         current_time = datetime.now()
         formatted_time = current_time.strftime("%d/%m/%Y %H:%M")
+	
+	# Generate a payload and POST to webhook
         payload = {"content": f"({formatted_time}) Raspberry pi online!\n{my_IP}"}
         request = requests.post(webhook, data=json.dumps(payload), headers={"Content-type" : "application/json"})
         break
